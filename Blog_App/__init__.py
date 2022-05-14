a763f06d8afe25d2b342ec912a87031d914cb918
HopeWiddows <CCC012108@coderacademy.edu.au>
2022-05-14 14:37:15 +1000

    updating

---
+++ Blog_App/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+#Setting secret key for encryption
+app.config['SECRET_KEY'] = 'd88f014831ffcc2ad154c6550f555332'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #adding database relative path within our project
+db = SQLAlchemy(app) #creating database instance
+
+#creating login manager for web app
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+
+from Blog_App import routes
+
+#enabling live debug mode/conditional within project
+if __name__ == '__main__':
+    app.run(debug=True)
