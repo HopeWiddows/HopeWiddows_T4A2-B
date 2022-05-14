1c8f9e7e3032287dfd7b89686946aeae70fb860c
HopeWiddows <CCC012108@coderacademy.edu.au>
2022-05-14 14:10:29 +1000

    added html templates (pre written in previous project)

--- Blog_App/Blog_App.py
+++ Blog_App/Blog_App.py
@@ -1,6 +1,17 @@
 from flask import Flask
 app = Flask(__name__)
 
+#creating home page
 @app.route("/")
-def hello():
+@app.route("/home")
+def home():
     return "<h1>Home Page</h1>"
+
+#creating new 'about' page
+@app.route("/about")
+def about():
+    return "<h1>About Page</h1>"
+
+#enabling live debug mode/conditional within project
+if __name__ == '__main__':
+    app.run(debug=True)
