0c411cd4950e4e4e04ad179c96ef1e7a2fd6a846
HopeWiddows <CCC012108@coderacademy.edu.au>
2022-05-14 17:00:09 +1000

    updates

--- Blog_App/__init__.py
+++ Blog_App/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 #Setting secret key for encryption
-app.config['SECRET_KEY'] = 'd88f014831ffcc2ad154c6550f555332'
+app.config['SECRET_KEY'] = '1eaaddc9ac588563a208d46f2098ee7d'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #adding database relative path within our project
 db = SQLAlchemy(app) #creating database instance
 
