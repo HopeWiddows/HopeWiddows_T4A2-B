7d06064236f2eb80b072aa81a06817dde1390db5
HopeWiddows <CCC012108@coderacademy.edu.au>
2022-05-17 16:14:32 +1000

    created init file

---
+++ Notes_App/__init__.py
@@ -0,0 +1,11 @@
+#importing/set up dependencies 
+from flask import Flask, flask
+
+#defining app function
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '1eaaddc9ac588563a208d46f2098ee7d'
+
+    return app
+
+
