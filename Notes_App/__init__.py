e7c67156326c968ec72b3c7002258388772185b1
HopeWiddows <CCC012108@coderacademy.edu.au>
2022-05-17 16:45:25 +1000

    created additional pages and routes

--- Notes_App/__init__.py
+++ Notes_App/__init__.py
@@ -6,6 +6,13 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '1eaaddc9ac588563a208d46f2098ee7d'
 
+    #importing routes to app files
+    from .routes import routes
+    from .auth import auth
+
+    app.register_blueprint(routes, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
 
 
--- Notes_App/auth.py
+++ Notes_App/auth.py
@@ -2,3 +2,15 @@ from flask import Blueprint
 
 #defining blueprint for authorisation 
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Log Out</p>"
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
